cf8269dd9bccaeddfb05efb8dcce5a1398272ab6
Bryson Florez <bryson.mosquera@linktic.com>
2023-09-10 20:11:34 -0500

    Initial commit

---
+++ src/main/java/com/linktic/login/RduMsLoginV1Application.java
@@ -0,0 +1,13 @@
+package com.linktic.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RduMsLoginV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RduMsLoginV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/login/RduMsLoginV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RduMsLoginV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
